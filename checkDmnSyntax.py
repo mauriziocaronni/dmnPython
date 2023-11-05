002f715021a24bb4cfb0a38c0fc7223e8cbe98a1
Caronni Maurizio <maurizio.caronni@beta80group.it>
2023-11-05 23:28:16 +0100

    aggiunto script per check sintassi

---
+++ checkDmnSyntax.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+import pyDMNrules
+import os
+import pandas as pd
+import glob
+
+pathDmn = './dmn/'
+pathTest = './tests/'
+
+fileName = 'Opening-checkCompletedData-011.dmn'
+# fileName = 'diagram.dmn'
+
+def checkDmn ( dmnFile, data ):
+    try:
+        print ('\n\nDMN file read: '+  dmnFile + ' - ' + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        # legge un'istanza della classe DMNModel
+        dmnRules = pyDMNrules.DMN()
+        status = dmnRules.loadXML(dmnFile)
+        print ('DMN file loaded:', status)
+        
+        (status, newData) = dmnRules.decide(data)
+        print('Decision',repr(newData))
+
+        # Extract result
+        key_value = newData['Result']   
+        print('Result:', key_value)
+        #return key_value
+    except Exception as e:
+        print("\t\t\tAn error occurred:", e)
+
+
+
+def main():
+    print("start", datetime.now())
+    
+    # set Data
+    #    keys = ['Azienda', 'Compagnia', 'DannoTipo','TipoIncarico']  
+    data = {}
+    data['Azienda'] = 'A&A'
+    data['Compagnia'] = 'Generali'
+    data['DannoTipo'] = 'Danno Elettrico'
+    #data['TipoIncarico'] = 'D'
+    #data['IntervalloDinizioDsinistro'] = 1
+
+    # leggi tutti i file dmn 
+    files = glob.glob(pathDmn + '*.dmn')
+
+    # esegui ciclo su tutti i file dmn
+    for file in files:
+        result = checkDmn (file, data)
+        
+    print("end", datetime.now())
+
+if __name__ == "__main__":
+    main()
